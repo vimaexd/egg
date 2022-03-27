7af3704c748a0e82417050911f0aeb500c3d3231
Mae <34780309+etstringy@users.noreply.github.com>
2022-03-27 00:09:51 +0000

    😳 Fix font

--- packages/omelette/src/pages/_document.tsx
+++ packages/omelette/src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
       <title>Eggbot</title>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
       <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'></link>
       <body>
         <Main />
