195fd0c4eedb5896a93b387054d725f7307ba7e2
etstringy <eddyteddyt@gmail.com>
2022-03-31 18:19:50 +0100

    Next config change

--- packages/omelette/next.config.js
+++ packages/omelette/next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['cdn.discordapp.com']
+  },
+  async redirects() {
+    return [
+      {
+        source: '/givemeyourmoney',
+        destination: 'https://www.youtube.com/watch?v=SdH13BXdNqI',
+        permanent: true
+      }
+    ]
   }
 }
 
